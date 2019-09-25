5c8a38e2afa9054c54a0333a2c794aa6398a5950
Srujan Mupparapu <srujanmup@gmail.com>
2019-09-25 09:45:33 -0500

    GameClass added

---
+++ Pig_Roulette/GameClass.swift
@@ -0,0 +1,26 @@
+//
+//  GameClass.swift
+//  Pig_Roulette
+//
+//  Created by Mupparapu, Srujan on 9/25/19.
+//  Copyright © 2019 Vadlakonda, Sujay V. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class PigGame {
+    let numberOfPlayers: Int
+    let goalPoints: Int
+    let maxDice: Int
+    init(numberOfPlayers: Int, goalPoints: Int, maxDice: Int) {
+        self.numberOfPlayers = numberOfPlayers
+        self.goalPoints = goalPoints
+        self.maxDice = maxDice
+        
+    }
+    
+}
+
+class Player { }
