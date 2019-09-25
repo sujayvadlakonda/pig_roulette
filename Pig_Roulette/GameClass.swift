ed56a6e6f0be6d7681b2aad7f97deb9e0d9eee8b
Srujan Mupparapu <srujanmup@gmail.com>
2019-09-25 10:11:19 -0500

    added functionality to pig game

--- Pig_Roulette/GameClass.swift
+++ Pig_Roulette/GameClass.swift
@@ -8,19 +8,53 @@
 
 import Foundation
 
-import Foundation
-
 class PigGame {
+    
     let numberOfPlayers: Int
     let goalPoints: Int
     let maxDice: Int
+    
+    var players: [Player]
+    var currentPlayer: Int
+    
     init(numberOfPlayers: Int, goalPoints: Int, maxDice: Int) {
         self.numberOfPlayers = numberOfPlayers
         self.goalPoints = goalPoints
         self.maxDice = maxDice
         
+        self.players = []
+        for num in 1...numberOfPlayers {
+            players.append(Player(name: "Player \(num)", points: 0))
+        }
+        currentPlayer = 0
+    }
+    
+    func roll() -> Int {
+        return Int.random(in: 1...6)
+    }
+    
+    func nextPlayer() {
+        currentPlayer += 1
+        if currentPlayer == numberOfPlayers {
+            currentPlayer = 0
+        }
+    }
+    
+    func updatePlayer(earnedPoints: Int) -> Bool {
+        players[currentPlayer].points += earnedPoints
+        return players[currentPlayer].points >= goalPoints
     }
     
 }
 
-class Player { }
+class Player {
+    
+    let name: String
+    var points: Int
+    
+    init(name: String, points: Int) {
+        self.name = name
+        self.points = points
+    }
+
+}
