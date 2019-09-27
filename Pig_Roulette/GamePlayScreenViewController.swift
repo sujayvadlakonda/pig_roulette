81b1b264f872ed3ee9a671b9c313cd166b728959
Srujan Mupparapu <srujanmup@gmail.com>
2019-09-27 09:36:02 -0500

    Started Main Game View

---
+++ Pig_Roulette/GamePlayScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GamePlayScreenViewController.swift
+//  Pig_Roulette
+//
+//  Created by Mupparapu, Srujan on 9/27/19.
+//  Copyright © 2019 Vadlakonda, Sujay V. All rights reserved.
+//
+
+import UIKit
+
+class GamePlayScreenViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
