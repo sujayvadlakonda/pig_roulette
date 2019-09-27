c22a8bf6f92b9bd4e38edf3d9ce767dc38117993
Srujan Mupparapu <srujanmup@gmail.com>
2019-09-27 10:15:30 -0500

    made outlets for main game screen

--- Pig_Roulette/GamePlayScreenViewController.swift
+++ Pig_Roulette/GamePlayScreenViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class GamePlayScreenViewController: UIViewController {
     
+    @IBOutlet weak var playerTitle: UILabel!
+    @IBOutlet weak var totalPoints: UILabel!
+    @IBOutlet weak var roundPoints: UILabel!
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    @IBOutlet weak var dice3: UIImageView!
+    @IBOutlet weak var dice4: UIImageView!
+    @IBOutlet weak var messageDisplay: UILabel!
+    @IBOutlet weak var rollButton: UIButton!
+    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var leaderboardButton: UIButton!
     
-
+    let game = PigGame(numberOfPlayers: 4, goalPoints: 100, maxDice: 4)
+    var numRoundPoints = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        playerTitle.text = "\(game.players[game.currentPlayer].name)'s Turn"
+        totalPoints.text = "\(game.players[game.currentPlayer].points)"
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
 
+    @IBAction func rollPress(_ sender: Any) {
+        var pointsSum = 0
+        for dieNum in 1...(game.maxDice) {
+            let val = game.roll()
+            if val == 1 {
+                messageDisplay.text = "You rolled a one, too bad!!! No points awarded."
+                return
+            }
+            pointsSum += val
+            if (dieNum == 1) {
+                // change image of dice1 to match rolled number
+            } else if (dieNum == 2) {
+                // change image of dice2 to match rolled number
+            } else if (dieNum == 3) {
+                // change image of dice3 to match rolled number
+            } else if (dieNum == 4) {
+                // change image of dice4 to match rolled number
+            }
+        }
+    }
 }
